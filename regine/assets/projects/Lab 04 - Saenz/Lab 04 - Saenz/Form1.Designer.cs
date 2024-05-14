8e561e968a0a9aae1f27c73e591bcaf453bcb758
Regine Saenz <saenzregine@gmail.com>
2024-05-15 05:20:14 +0800

    Initial commit with My Portfolio Website I

---
+++ regine/assets/projects/Lab 04 - Saenz/Lab 04 - Saenz/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace Lab_04___Saenz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtQty = new System.Windows.Forms.TextBox();
+            this.cboSize = new System.Windows.Forms.ComboBox();
+            this.cboDrinks = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstBill = new System.Windows.Forms.ListBox();
+            this.btnCompute = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnAdd);
+            this.groupBox1.Controls.Add(this.txtQty);
+            this.groupBox1.Controls.Add(this.cboSize);
+            this.groupBox1.Controls.Add(this.cboDrinks);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(50, 51);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Size = new System.Drawing.Size(834, 235);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Choose an Order!";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(650, 65);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(126, 71);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "Add Order";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtQty
+            // 
+            this.txtQty.Location = new System.Drawing.Point(444, 112);
+            this.txtQty.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtQty.Name = "txtQty";
+            this.txtQty.Size = new System.Drawing.Size(112, 27);
+            this.txtQty.TabIndex = 5;
+            this.txtQty.MouseClick += new System.Windows.Forms.MouseEventHandler(this.txtQty_MouseClick);
+            // 
+            // cboSize
+            // 
+            this.cboSize.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboSize.FormattingEnabled = true;
+            this.cboSize.Location = new System.Drawing.Point(155, 109);
+            this.cboSize.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.cboSize.Name = "cboSize";
+            this.cboSize.Size = new System.Drawing.Size(136, 27);
+            this.cboSize.TabIndex = 4;
+            this.cboSize.SelectedIndexChanged += new System.EventHandler(this.cboSize_SelectedIndexChanged);
+            // 
+            // cboDrinks
+            // 
+            this.cboDrinks.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboDrinks.FormattingEnabled = true;
+            this.cboDrinks.Location = new System.Drawing.Point(155, 43);
+            this.cboDrinks.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.cboDrinks.Name = "cboDrinks";
+            this.cboDrinks.Size = new System.Drawing.Size(401, 27);
+            this.cboDrinks.TabIndex = 3;
+            this.cboDrinks.SelectedIndexChanged += new System.EventHandler(this.cboDrinks_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(364, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Quantity:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Size:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Drinks:";
+            // 
+            // lstBill
+            // 
+            this.lstBill.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstBill.FormattingEnabled = true;
+            this.lstBill.ItemHeight = 20;
+            this.lstBill.Location = new System.Drawing.Point(50, 303);
+            this.lstBill.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.lstBill.Name = "lstBill";
+            this.lstBill.Size = new System.Drawing.Size(834, 244);
+            this.lstBill.TabIndex = 1;
+            // 
+            // btnCompute
+            // 
+            this.btnCompute.Enabled = false;
+            this.btnCompute.Location = new System.Drawing.Point(50, 557);
+            this.btnCompute.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnCompute.Name = "btnCompute";
+            this.btnCompute.Size = new System.Drawing.Size(139, 39);
+            this.btnCompute.TabIndex = 2;
+            this.btnCompute.Text = "Check Out";
+            this.btnCompute.UseVisualStyleBackColor = true;
+            this.btnCompute.Click += new System.EventHandler(this.btnCompute_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(195, 557);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(139, 39);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "New Transaction";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(344, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(267, 19);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "XYZ REfreshment Ordering System";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Black", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(905, -2);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 40);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "X";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(940, 627);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnCompute);
+            this.Controls.Add(this.lstBill);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "XYZ REfreshment Ordering System";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtQty;
+        private System.Windows.Forms.ComboBox cboSize;
+        private System.Windows.Forms.ComboBox cboDrinks;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstBill;
+        private System.Windows.Forms.Button btnCompute;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+    }
+}
+
---
+++ regine/assets/projects/Lab 04 - Saenz/Lab 04 - Saenz/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Lab_04___Saenz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Define product list
+        private string[,] strProducts = {
+                                            {"Wintermelon Special","135.00","155.00"},
+                                            {"Wintermelon Overload", "155.00", "175.00"},
+                                            {"Green Apple", "90.00", "100.00"},
+                                            {"Lychee", "90.00","100.00"},
+                                            {"Matcha", "110.00", "125.00"}};
+    
+        //Define enum size
+        enum DrinkSize
+        {
+            Regular = 1,
+            Large
+        };
+
+        private string fmtHead = "{0, -25}{1, -8}{2, 10}{3, 4}{4, 15}"; //Bill format
+        private decimal decTotal = 0M; //Class level total
+        private string strDrinks = "";
+        private decimal decPrice = 0M;
+        DrinkSize size; //Drink size
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Load each product name into the combobox
+            for (int i = 0; i < strProducts.GetLength(0); i++)
+            {
+                cboDrinks.Items.Add(strProducts[i,0]);
+            }
+            //Set the default selection to Wintermelon Special
+            cboDrinks.SelectedIndex = 0;
+
+            //Assign size to combobox
+            cboSize.Items.Add(DrinkSize.Regular);
+            cboSize.Items.Add(DrinkSize.Large);
+            //Set the default selection to Wintermelon Regular
+            cboSize.SelectedIndex = 0;
+
+            //Show Receipt header
+            lstBill.Items.Add("XYZ Refreshments");
+            lstBill.Items.Add("Customer Bill");
+            lstBill.Items.Add("");
+            lstBill.Items.Add(string.Format(fmtHead, "Items", "Size", "Price", "Qty", "Sub Total"));
+            lstBill.Items.Add("--------------------------------------------------------------------------------------------------------------------");
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+            if (txtQty.Text.Trim() == string.Empty)
+            {
+                MessageBox.Show("Enter the number of Drinks" +
+                    "\n", "Error!", MessageBoxButtons.OK,
+                    MessageBoxIcon.Exclamation);
+                txtQty.Text = "";
+                txtQty.Focus();
+                return;
+            }
+                decimal Quantity = .0m;
+                btnCompute.Enabled = true;
+                try
+                {
+                    Quantity = decimal.Parse(txtQty.Text);
+                }
+
+                catch (Exception)
+                {
+                    MessageBox.Show("Check and Enter the Number of Drinks" +
+                        "\n", "Error!", MessageBoxButtons.OK,
+                        MessageBoxIcon.Exclamation);
+                    txtQty.Text = "";
+                    txtQty.Focus();
+                    return;
+                }
+
+                int qty = 0;
+                qty = int.Parse(txtQty.Text);
+                decimal subtotal = 0M;
+
+                switch (strDrinks)
+                {
+                    case "Wintermelon Special":
+                        // get drink price based on size
+                        if (size == DrinkSize.Regular)
+                            decPrice = 135;
+                        else
+                            decPrice = 155;
+                        break;
+
+                    case "Wintermelon Overload":
+                        // get drink price based on size
+                        if (size == DrinkSize.Regular)
+                            decPrice = 155;
+                        else
+                            decPrice = 175;
+                        break;
+
+                    case "Green Apple":
+                        // get drink price based on size
+                        if (size == DrinkSize.Regular)
+                            decPrice = 90;
+                        else
+                            decPrice = 100;
+                        break;
+
+                    case "Lychee":
+                        // get drink price based on size
+                        if (size == DrinkSize.Regular)
+                            decPrice = 90;
+                        else
+                            decPrice = 100;
+                        break;
+
+                    case "Matcha":
+                        // get drink price based on size
+                        if (size == DrinkSize.Regular)
+                            decPrice = 110;
+                        else
+                            decPrice = 125;
+                        break;
+                    //Add cases for other products
+                }
+                subtotal = decPrice * qty; //compute sub total
+                //increment total
+                decTotal += subtotal;
+                //add item to receipt
+                lstBill.Items.Add(string.Format(fmtHead, strDrinks,
+                    size, decPrice.ToString("n2"), qty.ToString("n1"),
+                    subtotal.ToString("n2")));
+            
+        }
+
+        private void btnCompute_Click(object sender, EventArgs e)
+        {
+        btnCompute.Enabled = false;
+        lstBill.Items.Add("");
+        lstBill.Items.Add("--------------------------------------------------------------------------------------------------------------------------");
+        decimal tax = decTotal * .1M;
+        lstBill.Items.Add(string.Format(fmtHead, "", "", "TAX", "", tax.ToString("n2")));
+        decTotal += tax;
+        lstBill.Items.Add(string.Format(fmtHead, "", "", "TOTAL", "", decTotal.ToString("n2")));
+
+            //printpreview
+         printPreviewDialog1.Document = printDocument1;
+         printPreviewDialog1.PrintPreviewControl.Zoom = 1.5;
+         printPreviewDialog1.ShowDialog();
+        }
+
+        //double-click cboDrinks to automatically generate this method
+        //fill required code
+        private void cboDrinks_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            strDrinks = cboDrinks.Text;
+        }
+
+        //double click cboSize to automatically generate this method
+        //fill the required code
+        private void cboSize_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            size = (DrinkSize)cboSize.SelectedIndex + 1; // assign size
+            //call the GetPrice method Identify the price
+            decPrice = GetPrice(cboSize.SelectedIndex, size);
+        }
+
+        private decimal GetPrice(int intDrink,DrinkSize size)
+        {
+ 	        decimal price = 0M;
+            if (size == DrinkSize.Regular)
+                price = decimal.Parse(strProducts[intDrink,1]);
+            else
+                price = decimal.Parse(strProducts[intDrink, 2]);
+            return price;
+}
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            decTotal = .0M;
+            lstBill.Items.Clear();
+            btnCompute.Enabled = false;
+
+            lstBill.Items.Add("XYZ Refreshments");
+            lstBill.Items.Add("Customer Bill");
+            lstBill.Items.Add("");
+            lstBill.Items.Add(string.Format(fmtHead, "Items", "Size", "Price", "Qty", "Sub Total"));
+            lstBill.Items.Add("-------------------------------------------------------------------------------------------------------------------------------");
+        }
+
+        private void txtQty_MouseClick(object sender, MouseEventArgs e)
+        {
+            txtQty.SelectAll();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Are you sure you want to close the application?" + "\n", "Exit", MessageBoxButtons.OK);
+          
+            this.Close();
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            int StartX = 65;
+            int StartY = 65;
+
+            for (int x = 0; x < lstBill.Items.Count; x++)
+            {
+                e.Graphics.DrawString(lstBill.Items[x].ToString(), lstBill.Font, Brushes.Black, StartX, StartY);
+                StartY += lstBill.ItemHeight;
+            }
+            
+            }
+
+
+       
+
+}
+}
